ad84b6a64d0c08b102a6fd65da60dde88f2d3c74
OMiD <o.rasouli92@gmail.com>
2025-08-04 20:30:56 +0330

    Wiring project

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"strconv"
 
+	"github.com/OmidRasouli/weather-api/infrastructure/database"
 	"github.com/OmidRasouli/weather-api/infrastructure/database/postgres"
+	"github.com/OmidRasouli/weather-api/internal/application/services"
 	configs "github.com/OmidRasouli/weather-api/internal/configs"
-	database "github.com/OmidRasouli/weather-api/internal/database/migrations"
+	databaseMigration "github.com/OmidRasouli/weather-api/internal/database/migrations"
+	postgresRepo "github.com/OmidRasouli/weather-api/internal/infrastructure/database/postgres/weather"
+	"github.com/OmidRasouli/weather-api/internal/infrastructure/openweather"
+	"github.com/OmidRasouli/weather-api/internal/interfaces/http/controller"
 	router "github.com/OmidRasouli/weather-api/internal/interfaces/http/routers"
 	"github.com/OmidRasouli/weather-api/pkg/logger"
 )
@@ -13,25 +18,26 @@ import (
 func main() {
 	logger.Info("The application is starting...")
 	cfg := configs.MustLoad("config/config.yaml")
-	RunServer(cfg)
+	db := RunDatabase(cfg)
+	RunServer(cfg, db)
 }
 
-func RunServer(cfg *configs.Config) {
-	// Set up the HTTP router and register all routes.
-	router := router.Setup()
-
-	// Build the server address string using the configured port.
+func RunServer(cfg *configs.Config, db database.Database) {
+	weatherRepo := postgresRepo.NewWeatherPostgresRepository(db)
+	apiClient := openweather.NewClient(cfg.GetOpenWeatherConfig().APIKey)
+	weatherService := services.NewWeatherService(weatherRepo, apiClient)
+	weatherController := controller.NewWeatherController(weatherService)
+	r := router.Setup(weatherController)
 	port := cfg.GetServerConfig().Port
 	addr := ":" + strconv.Itoa(port)
-
 	logger.Infof("Server is starting on port %d", port)
-	// Start the HTTP server; log an error if the server fails to start.
-	if err := router.Run(addr); err != nil {
+
+	if err := r.Run(addr); err != nil {
 		logger.Errorf("failed to start server: %v", err)
 	}
 }
 
-func RunDatabase(cfg *configs.Config) {
+func RunDatabase(cfg *configs.Config) database.Database {
 	// Create a new database connection using the configuration values.
 	db, err := postgres.NewPostgresConnection(postgres.PostgresConfig{
 		Host:     cfg.GetDatabaseConfig().Host,
@@ -46,16 +52,18 @@ func RunDatabase(cfg *configs.Config) {
 	}
 
 	// Create a new migration instance for managing database migrations.
-	migrationInstance, err := database.NewMigrateInstance(db, "db/migrations", cfg.GetDatabaseConfig().DBName)
+	migrationInstance, err := databaseMigration.NewMigrateInstance(db, "db/migrations", cfg.GetDatabaseConfig().DBName)
 	if err != nil {
 		logger.Errorf("failed to create migration instance: %v", err)
 	}
 
 	// Initialize the MigrationManager with the database connection and migration instance.
-	migrationManager := database.NewMigrationManager(db, migrationInstance)
+	migrationManager := databaseMigration.NewMigrationManager(db, migrationInstance)
 
 	// Run all pending database migrations and log any errors.
 	if err := migrationManager.RunMigrations(); err != nil {
 		logger.Errorf("failed to run migrations: %v", err)
 	}
+
+	return db
 }
